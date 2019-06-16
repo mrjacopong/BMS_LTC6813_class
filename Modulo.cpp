462969d190341e3e2bd79584d7a22e4aabbbfb04
Diba98 <48396268+Diba98@users.noreply.github.com>
2019-06-16 20:25:27 +0200

    Inserimento funzione stampa per grafici

--- Modulo.cpp
+++ Modulo.cpp
@@ -40,3 +40,20 @@ bool Modulo::carica(cell_asic bms_ic[],int modulo_corrente){
     }
 
 }
+
+void StampaVoltaggio (cell_asic bms_ic[],int modulo_corrente){  //stampa nel monitor seriale di arduino
+    for (int i=0;i<n_celle;i++){
+        //salta la cella  unused_ch_1=9 e unused_ch_2=18
+        //queste due celle sono sempre a 0v e non sono un errore
+        if ((modulo_corrente!= unused_ch_1) || (modulo_corrente != unused_ch_2)){
+            Serial.println(bms_ic[modulo_corrente].cells.c_codes[i]+"V");
+        }
+}
+void StampaTemp (cell_asic bms_ic[],int modulo_corrente){   //stampa nel monitor seriale di arduino
+    for (int i=0;i<n_celle;i++){
+        //salta la cella  unused_ch_1=9 e unused_ch_2=18
+        //queste due celle sono sempre a 0v e non sono un errore
+        if ((modulo_corrente!= unused_ch_1) || (modulo_corrente != unused_ch_2)){
+            Serial.println(bms_ic[modulo_corrente].aux.a_codes[j]+"V");
+        }
+}
