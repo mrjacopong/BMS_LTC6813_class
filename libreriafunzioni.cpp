404c893258b87d5ab8b0f180025cce0c7c389487
MassimoCiaffoni <ciaffoni.massimo98@gmail.com>
2019-06-18 18:21:23 +0200

    aggiunti accensione e spegnimento led errori e bilanciamento

--- libreriafunzioni.cpp
+++ libreriafunzioni.cpp
@@ -12,6 +12,7 @@ uint16_t IsTop(uint16_t top,uint16_t actual){                            //ritor
 
 void shoutdown_error(uint8_t pinOut){
   open_relay(pinOut);
+  AccendiLed(LedErrore);
 }
 bool time_check(unsigned long t_inizio ,uint16_t durata_max ){        //true se l'errore persiste
   /*controllo della durata dell'errore*/
@@ -45,11 +46,13 @@ void final_balance(uint16_t tensione,uint8_t pinOut,cell_asic bms_ic[],int8_t mo
 
 void greater_balance(uint16_t tensoine_iniziale,uint8_t pinOut,cell_asic bms_ic[],uint8_t modulo,uint8_t cella){
   open_relay(pinOut);
+  AccendiLed(LedBilanciamentoPesante);
   while (tensoine_iniziale - bms_ic[modulo].cells.c_codes[cella] >= delta_carica){
     set_discharge(cella,bms_ic);
     voltage_measurment(bms_ic);
   } //finquando la tensione attuale non si abbassa di un delta definito da noi non esce dal ciclo
   close_relay(pinOut);
+  SpegniLed(LedBilanciamentoPesante);
 }
 
 void intermediate_balance(int8_t cella,cell_asic bms_ic[]){
